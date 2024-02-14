a692ee1604ec74a200594a1d724d11c3d7b75c91
Aamiz <aamiz.sameen@axcess.io>
2024-02-14 21:14:39 +0530

    Update buildImage

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,7 +2,7 @@
 def call(BUILD_IMAGE_NAME) {
     script {
         // Use the source command to run count_script.sh in the same shell
-        TAG_ID = sh(script: '. ../imageTagId.sh', returnStdout: true).trim()
+        TAG_ID = sh(script: '. ../vars/imageTagId.sh', returnStdout: true).trim()
 
         // Use the new count in the docker build command
         sh "docker build -t ${env.BUILD_IMAGE_NAME}:${TAG_ID} ."
