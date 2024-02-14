9afd55c03b5603eb72e9df16cea6f1facc87e259
Aamiz <aamiz.sameen@axcess.io>
2024-02-14 21:31:51 +0530

    Updated pushImage

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,8 +1,8 @@
 //The image built from buildImage is pushed to ECR with proper tag
 def call(REGION, REPO_LOGIN, REPO_URL, BUILD_IMAGE_NAME) {
 	script {
-
-	TAG_ID="../vars/tagId.txt"
+	
+	TAG_ID = File(../vars/tagId.txt).text.trim()
         sh "aws ecr get-login-password --region ${env.REGION} | docker login --username AWS --password-stdin ${env.REPO_LOGIN}"
         sh "docker tag ${env.BUILD_IMAGE_NAME}:${TAG_ID} ${env.REPO_URL}:${TAG_ID}"
         sh "docker push ${env.REPO_URL}:${TAG_ID}"
