1b0ec929d25e2da8a91af8b50ff766bdb2ea6703
Aamiz <aamiz.sameen@axcess.io>
2024-02-15 10:57:29 +0530

    update pushImage

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,5 +1,5 @@
 //The image built from buildImage is pushed to ECR with proper tag
-def call(REGION, REPO_LOGIN, REPO_URL, BUILD_IMAGE_NAME, TAG_ID) {
+def call(REGION, REPO_LOGIN, REPO_URL, BUILD_IMAGE_NAME) {
 	script {
 	
 	//def FILE = '../vars/tagId.txt'
@@ -21,10 +21,10 @@ def call(REGION, REPO_LOGIN, REPO_URL, BUILD_IMAGE_NAME, TAG_ID) {
 	//sh "TAG_ID=$(cat ../vars/tagId.txt)"
 	//sh TAG_ID='cat ../vars/tagId.txt'
 	//sh "echo $NEW_COUNT"
-	//TAG_ID = new File(../vars/tagId.txt).text.trim()
+	TAG_ID = new File(../vars/tagId.txt).text.trim()
         sh "aws ecr get-login-password --region ${env.REGION} | docker login --username AWS --password-stdin ${env.REPO_LOGIN}"
-        sh "docker tag ${env.BUILD_IMAGE_NAME}:${env.TAG_ID} ${env.REPO_URL}:${env.TAG_ID}"
-        sh "docker push ${env.REPO_URL}:${env.TAG_ID}"
+        sh "docker tag ${env.BUILD_IMAGE_NAME}:${TAG_ID} ${env.REPO_URL}:${TAG_ID}"
+        sh "docker push ${env.REPO_URL}:${TAG_ID}"
 
 	}
 }
