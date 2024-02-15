be25b71ca47474644977923e6bfb37f053a60bf8
Aamiz <aamiz.sameen@axcess.io>
2024-02-15 09:50:50 +0530

    update pushImage

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -23,7 +23,7 @@ def call(REGION, REPO_LOGIN, REPO_URL, BUILD_IMAGE_NAME) {
 
 	//TAG_ID = new File(../vars/tagId.txt).text.trim()
         sh "aws ecr get-login-password --region ${env.REGION} | docker login --username AWS --password-stdin ${env.REPO_LOGIN}"
-        sh "docker tag ${env.BUILD_IMAGE_NAME}:${TAG_ID} ${env.REPO_URL}:${TAG_ID}"
+        sh "docker tag ${env.BUILD_IMAGE_NAME}:${NEW_COUNT} ${env.REPO_URL}:${NEW_COUNT}"
         sh "docker push ${env.REPO_URL}:${NEW_COUNT}"
 
 	}
