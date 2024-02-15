f4bee8dea63a041e325e18ffd4dec0448ce682cc
Aamiz <aamiz.sameen@axcess.io>
2024-02-15 11:13:24 +0530

    update pushImage

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -22,9 +22,11 @@ def call(REGION, REPO_LOGIN, REPO_URL, BUILD_IMAGE_NAME) {
 	//sh TAG_ID='cat ../vars/tagId.txt'
 	//sh "echo $NEW_COUNT"
 	//TAG_ID = new File(../vars/tagId.txt).text.trim()
+	//variableToGet = sh(returnStdout: true, script: """. ${DIRECTORY}/bash_file.sh echo \$VARIABLE""").trim()
+		TAG_ID=sh(returnStdout: true, script: """cat ../vars/tagId.txt""").trim()
         	sh "aws ecr get-login-password --region ${env.REGION} | docker login --username AWS --password-stdin ${env.REPO_LOGIN}"
-        	sh "docker tag ${env.BUILD_IMAGE_NAME}:${env.$TAG_ID} ${env.REPO_URL}:${env.$TAG_ID}"
-        	sh "docker push ${env.REPO_URL}:${env.$TAG_ID}" 
+        	sh "docker tag ${env.BUILD_IMAGE_NAME}:${TAG_ID} ${env.REPO_URL}:${TAG_ID}"
+        	sh "docker push ${env.REPO_URL}:${TAG_ID}" 
 
 	}
 }
