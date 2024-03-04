1dfe1f60bcdbe613c0ffdccc3626a9a5e5f4a6d2
Aamiz <aamiz.sameen@axcess.io>
2024-03-04 20:35:07 +0530

    Jenkinsfile updated

--- vars/slackNotification.groovy
+++
@@ -1,9 +0,0 @@
-
-def call() {
-      echo "Sending message to Slack"
-      slackSend (color: "danger",
-                 channel: "jenkins-notifications",
-                 message: "*FAILED:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}")
-}
-
-
