6f5f2d579c51d7561546af6403090d483fbd7dc6
aamizsameen <43962517+aamizsameen@users.noreply.github.com>
2024-03-11 08:12:37 +0530

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,12 +1,13 @@
-//The image built from buildImage is pushed to ECR with proper tag
-def call(REGION, REPO_LOGIN, REPO_URL, BUILD_IMAGE_NAME) {
-	script {
-	
-	//def FILE = '../vars/tagId.txt'
-		TAG_ID=sh(returnStdout: true, script: """cat ../vars/tagId.txt""").trim()
-        	sh "aws ecr get-login-password --region ${env.REGION} | docker login --username AWS --password-stdin ${env.REPO_LOGIN}"
-        	sh "docker tag ${env.BUILD_IMAGE_NAME}:${TAG_ID} ${env.REPO_URL}:${TAG_ID}"
-        	sh "docker push ${env.REPO_URL}:${TAG_ID}" 
+def call(REGION, REPO_LOGIN, REPO_URL, BUILD_IMAGE_NAME, DEPLOYMENT_NAME, NAMESPACE) {
+        script {
+		
+                TAG_ID = sh(script: """
+                       kubectl get deployment \${DEPLOYMENT_NAME} -n \${NAMESPACE} -o=jsonpath='{.spec.template.spec.containers[0].image}' | awk -F':' '{printf "%.1f", \$2 + 0.1}'
+                """, returnStdout: true).trim()
 
-	}
+                sh "aws ecr get-login-password --region ${env.REGION} | docker login --username AWS --password-stdin ${env.REPO_LOGIN}"
+                sh "docker tag ${env.BUILD_IMAGE_NAME}:${TAG_ID} ${env.REPO_URL}:${TAG_ID}"
+                sh "docker push ${env.REPO_URL}:${TAG_ID}"
+
+        }
 }
