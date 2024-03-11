e8c19340ed70e4a62f856a126327ed7a39f808a5
aamizsameen <43962517+aamizsameen@users.noreply.github.com>
2024-03-11 08:11:17 +0530

    Update imageCleanup.groovy

--- vars/imageCleanup.groovy
+++ vars/imageCleanup.groovy
@@ -1,8 +1,5 @@
 def call(BUILD_IMAGE_NAME) {
-	script {
-        TAG_ID=sh(returnStdout: true, script: """cat ../vars/tagId.txt""").trim()
-		sh "docker rmi -f ${env.BUILD_IMAGE_NAME}:${TAG_ID}"
-	}
+        script {
+                sh "docker system prune -af"
+        }
 }
-
-
