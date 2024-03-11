a136800bce10ddd6d81db047ec4fe8fb2455fd9c
aamizsameen <43962517+aamizsameen@users.noreply.github.com>
2024-03-11 08:10:39 +0530

    Update deployImage.groovy

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,10 +1,9 @@
-//Using the newly created Task Definition we create a new deployment
 def call(DEPLOYMENT_NAME, REPO_URL, NAMESPACE, CONTAINER_NAME) {
-	script {
-        TAG_ID=sh(returnStdout: true, script: """cat ../vars/tagId.txt""").trim()
-		sh "kubectl set image deployment/${env.DEPLOYMENT_NAME} ${env.CONTAINER_NAME}=${env.REPO_URL}:${TAG_ID} -n ${env.NAMESPACE}"
-		sh "kubectl rollout restart deployment/${env.DEPLOYMENT_NAME} -n ${env.NAMESPACE}"
-	}
+        script {
+                TAG_ID = sh(script: """
+                       kubectl get deployment \${DEPLOYMENT_NAME} -n \${NAMESPACE} -o=jsonpath='{.spec.template.spec.containers[0].image}' | awk -F':' '{printf "%.1f", \$2 + 0.1}'
+                """, returnStdout: true).trim()
+                sh "kubectl set image deployment/${env.DEPLOYMENT_NAME} ${env.CONTAINER_NAME}=${env.REPO_URL}:${TAG_ID} -n ${env.NAMESPACE}"
+                sh "kubectl rollout restart deployment/${env.DEPLOYMENT_NAME} -n ${env.NAMESPACE}"
+        }
 }
-
-
