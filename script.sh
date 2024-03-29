6d82c3a8e7265ea3174a9f08d82ff4b2da046518
aamizsameen <43962517+aamizsameen@users.noreply.github.com>
2024-03-29 15:10:03 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,3 +4,5 @@
 TAG_ID=$(kubectl get deployment ${DEPLOYMENT_NAME} -n ${NAMESPACE} -o=jsonpath='{.spec.template.spec.containers[0].image}' | awk -F':' '{printf "%.1f", $2 + 0.1}')
 
 echo $TAG_ID
+
+export $TAG_ID
