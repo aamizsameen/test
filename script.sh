b4a7219f57b5eb43808956bff7be689d9fc5a704
aamizsameen <43962517+aamizsameen@users.noreply.github.com>
2024-03-31 09:50:14 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-export TAG_ID=$(kubectl get deployment ${DEPLOYMENT_NAME} -n ${NAMESPACE} -o=jsonpath='{.spec.template.spec.containers[0].image}' | awk -F':' '{printf "%.1f", $2 + 0.1}')
-
-echo $TAG_ID
-
